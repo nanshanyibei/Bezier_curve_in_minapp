623fec8881546aa260528c7d82b8d63176a4430b
[nanshanyibei] <[1641685360@qq.com]>
2019-08-04 15:50:28 +0800

    初始化贝塞尔曲线

---
+++ pages/index/index.js
@@ -0,0 +1,52 @@
+//index.js
+Page({
+  data: {
+    leftAnimationShow: false,
+    rightAnimationShow: false,
+    leftAnimation: [
+      'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=2510163673,100790501&fm=26&gp=0.jpg',
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1564898475146&di=856669a32a74f9cbe8b445a3930dad48&imgtype=0&src=http%3A%2F%2Fimg5.duitang.com%2Fuploads%2Fitem%2F201408%2F25%2F20140825000809_dme4E.jpeg',
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1564898475145&di=6e2fe945935753bb69541e4aca108612&imgtype=0&src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fq_70%2Cc_zoom%2Cw_640%2Fimages%2F20190318%2Ff61ffcfed19c4e3aae35953ad63586d7.jpeg',
+      'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=4171782178,2970106987&fm=26&gp=0.jpg',
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1564898578359&di=d1000ff22a871641636ad9bdf44e0149&imgtype=0&src=http%3A%2F%2Fimages.liqucn.com%2Fimg%2Fh1%2Fh991%2Fimg201712091054090_info400X400.jpg',
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1564898599561&di=70f04e0b84293944ce6f6626a5843a84&imgtype=0&src=http%3A%2F%2Fimages.liqucn.com%2Fimg%2Fh1%2Fh966%2Fimg201709181055480_info300X300.jpg',
+      'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1564898638141&di=ee6ef88905af49c664ba708a03893c0a&imgtype=0&src=http%3A%2F%2Fwww.tbw-xie.com%2FtuxieJDEwLmFsaWNkbi5jb20vaTIvMjA0MDk0NzQ4MC9UQjIzUTl6c2lDWUJ1TmtTbmFWWFhjTXNWWGFfISEyMDQwOTQ3NDgwJDk.jpg',
+      'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2054029736,963620309&fm=26&gp=0.jpg'
+    ],
+    rightAnimation: [
+      {
+        avatar: 'https://timgsa.baidu.com/timg?image&quality=80&size=b10000_10000&sec=1564888769&di=c74fc6d965d1ab989c38dd84aa58c1fc&src=http://www.ghost64.com/qqtupian/zixunImg/local/2016/12/09/14812558367740.jpg',
+        text: '不知道发什么评论，反正我是凑字数的'
+      },{
+        avatar: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1564898724316&di=a626e0a36343d432a99130823b633d67&imgtype=0&src=http%3A%2F%2Fuploads.xuexila.com%2Fallimg%2F1609%2F676-160ZG51427-50.jpg',
+        text: '楼上好，我也是凑字数的'
+      },{
+        avatar: 'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=4003613396,4254608602&fm=26&gp=0.jpg',
+        text: '这么巧，看来大家都是凑字数的哈'
+      },{
+        avatar: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1564898801280&di=7924a3351d146fa0fbe2fd84b2d08af3&imgtype=0&src=http%3A%2F%2Fimg.duoziwang.com%2F2016%2F09%2F02%2F0827558796.jpg',
+        text: '来一个超长内容，啊啊啊啊啊啊啊啊啊啊啊啊，还是不知道说啥'
+      },{
+        avatar: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1565493539&di=565a82ca5035c11bb1a313179fbeeb77&imgtype=jpg&er=1&src=http%3A%2F%2Fimg.duoziwang.com%2Fuploads%2F1707%2F1-1FG02204080-L.jpg',
+        text: '一二三四五六七，七六五四三二一'
+      },{
+        avatar: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1565493555&di=8aa6df0e8d717894abf672a1e4b51fe3&imgtype=jpg&er=1&src=http%3A%2F%2Fimg.duoziwang.com%2Fuploads%2F1512%2F1-1512261603200-L.jpg',
+        text: '一二三四，二二三四，三二三四，四二三四'
+      }
+    ]
+  },
+  onLoad(){
+    console.log("页面渲染才完成")
+    setTimeout(() => {
+      this.setData({
+        leftAnimationShow: true
+      })
+    }, 2000)
+  },
+  leftAnimationEnd(e){
+    console.log(e)
+  },
+  rightAnimationEnd(e){
+    console.log(e)
+  }
+})
